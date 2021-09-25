a757155ec0be02ed16b428112c0429a4b43a8b32
Akshay Meshram <akshay.d7769@gmail.com>
2021-09-25 15:15:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import AddTask from './Components/AddTask';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import Register from './Components/Register';
+import UpdateTask from './Components/UpdateTask';
+import ViewTask from './Components/ViewTask';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/registration">
+            <Register />
+          </Route>
+          <Route path="/addtask">
+            <AddTask />
+          </Route>
+          <Route path="/viewtask">
+            <ViewTask />
+          </Route>
+          <Route path="/updatetask/:id">
+            <UpdateTask />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Register.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Register() {
+    const [user, setUser] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const reghandleInputChange = (e) =>{
+        setUser({ ...user, [e.target.name]: e.target.value})
+    }
+
+    const reghandleClick = () =>{
+        console.log(user);
+    }
+
+    return (
+        <div className="container">
+            <div className="register-form">
+                <h2 className="register-heading">Registration</h2>
+                <form action="">
+                    <div className="register-name space-bottom">
+                        <label className="register-namelabel" htmlFor="name">Name:</label><br />
+                        <input className="register-nameinput" type="text" name="name" id="name" value={user.name} onChange={reghandleInputChange} placeholder="Enter Name"/>
+                    </div>
+
+                    <div className="register-email space-bottom">
+                        <label className="register-emaillabel" htmlFor="email">Email:</label><br />
+                        <input className="register-emailinput" type="email" name="email" id="email" value={user.email} onChange={reghandleInputChange} placeholder="Enter Email" />
+                    </div>
+
+                    <div className="register-password space-bottom">
+                        <label className="register-passwordlabel" htmlFor="password">Password:</label><br />
+                        <input className="register-passwordinput" type="password" name="password" id="password" value={user.password} onChange={reghandleInputChange} placeholder="Enter Password" />
+                    </div>
+
+                    <Link to="/" exact>
+                        <div className="d-grid gap-2 col-12 mx-auto mt-2">
+                            <button type="submit" className="btn btn-primary btn-sm mt-2 btn-register" onClick={reghandleClick}>Register</button>
+                        </div>
+                    </Link>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
